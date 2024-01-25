8cf83e64616fe0b4598218f9ebbce20a15d09efd
Ivan Ukhov <ivan.ukhov.work@gmail.com>
2024-01-25 22:35:38 +0600

    fixed no 'from' case

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import json
+import os
 from datetime import datetime
 
 
@@ -9,10 +10,11 @@ def sorting_function(x):
 
 def read_operations():
     """Функция считывает операции в файле operations.json, фильтрует и сортирует, возвращая список"""
-    with open('../operations.json', 'r', encoding='utf-8') as file:
+    file_path = os.path.join("..", "operations.json")
+    with open(file_path, 'r', encoding='utf-8') as file:
         operations = json.load(file)
         executed_operations = []
-        needed_keys = ['date', 'from', 'to']
+        needed_keys = ['date']
         for operation in operations:
             if all([key in operation.keys() for key in needed_keys]) and operation['state'] == 'EXECUTED':
                 executed_operations.append(operation)
@@ -27,7 +29,7 @@ def formating_func(transaction):
     op_type = information[-1]
     if information[0] == 'Счет':
         return f"{name} **{op_type[-4:]}"
-    return f"{name} {op_type[:4]} {op_type[4:6]}** {'*' * 4} {op_type[-4:]} "
+    return f"{name} {op_type[:4]} {op_type[4:6]}** {'*' * 4} {op_type[-4:]}"
 
 
 def print_five_last_operations():
@@ -39,5 +41,8 @@ def print_five_last_operations():
         date_f = operation_date.strftime('%d.%m.%Y')
         amount = operation['operationAmount']
         print(f"{date_f} {operation['description']}")
-        print(f"{formating_func(operation['from'])} -> {formating_func(operation['to'])}")
+        if 'from' in operation.keys():
+            print(f"{formating_func(operation['from'])} -> {formating_func(operation['to'])}")
+        else:
+            print(f"-> {formating_func(operation['to'])}")
         print(f"{amount['amount']} {amount['currency']['name']}\n")
